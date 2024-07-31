40ba84c33c286c24a93c6cc9472c7f1c3ee76e34
Cobus <cobusreyneke1004@gmail.com>
2024-07-31 10:38:01 +0200

    [#6]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,17 @@
-public class Greeter {
-	public static void main (String[] args) {
+public class Greeter { 
 
-	}
-}
+public static void main(String[] args) { 
+
+greeter(args[0]); 
+
+} 
+
+  
+
+public static void greeter(String name) { 
+
+System.out.println("Good day my friend, " + name + "\n" + "Just imagine like are on 	drugs that this line is longer than it actually is"); 
+
+} 
+
+} 
