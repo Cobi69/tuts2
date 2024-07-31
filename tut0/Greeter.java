c8e209466fa58c0c7083aa6c19b5f9ae2dfdabb2
Cobus <cobusreyneke1004@gmail.com>
2024-07-31 11:28:27 +0200

    [#16] javadocs

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,32 +1,41 @@
-import java.util.Scanner; 
-public class Greeter { 
+/**
+ * The Greeter class provides methods to greet and say goodbye to the user.
+ */
+public class Greeter {
+    /**
+     * Main method to execute the greeter and goodbye functions.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        String name = getName();
+        greeter(name);
+        goodbye(name);
+    }
 
-public static void main(String[] args) { 
+    /**
+     * Gets the user's name from input.
+     * @return the user's name
+     */
+    public static String getName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        return scanner.nextLine();
+    }
 
-	greeter(); 
->>>>>>> feature_input_greeter
-} 
+    /**
+     * Prints a greeting to the user.
+     * @param name the user's name
+     */
+    public static void greeter(String name) {
+        System.out.println("Greetings, " + name + "! Welcome to our program.");
+    }
 
-  
-
-public static String getName() { 
-
-Scanner sc = new Scanner(System.in); 
-		System.out.print(“Please provide your name: “); 
-		String name = sc.nextLine(); 
-		return name;		 
+    /**
+     * Prints a parting message to the user.
+     * @param name the user's name
+     */
+    public static void goodbye(String name) {
+        System.out.println("Goodbye, " + name + "! Have a great day.");
+    }
 }
 
-public static void greeter() { 
-String name = getName();
-System.out.println("Good day my friend, " + name + "\n" + "Just imagine like are on	drugs that this line is longer than it actually is"); 
-
-} 
- 
-
-public static void greeter(String name) { 
-
-System.out.println("Good day my friend, " + name + "\n" + "Just imagine like are on 	drugs that this line is longer than it actually is"); 
-
-} 
-}
